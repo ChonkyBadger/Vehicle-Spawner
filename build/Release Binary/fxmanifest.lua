562243bff7946b478fa6e60af6f42b932624039a
Badger <george2005adams@gmail.com>
2023-03-17 09:52:36 +0000

    Removed unused value from config.json

---
+++ build/Release Binary/fxmanifest.lua
@@ -0,0 +1,20 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Vehicle Menu'
+author 'Badger'
+description 'BadgerCaveDevelopment@gmail.com'
+version '1.0'
+url 'https://github.com/ChonkyBadger/'
+
+-- Files
+client_script 'Client.net.dll'
+server_script 'Server.net.dll'
+
+
+files {
+    'MenuAPI.dll',
+    'Newtonsoft.Json.dll',
+    'config/config.json',
+}
